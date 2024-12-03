a8132b9167fed552a665f899b574904b756888a5
Austin Barrett <austinjbarrett98@gmail.com>
2024-12-03 00:57:24 -0500

    feat: init new repo

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'charcoal': '#0E0E0E',
+        'cream': '#F8F1F1',
+        'rose':'#2C1919',
+      },
+    },
+  },
+  plugins: [],
+};
