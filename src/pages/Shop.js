6c8b84b4858fb6c1f6d449b1739e97b93132f21b
Austin Barrett <austinjbarrett98@gmail.com>
2024-12-05 18:20:38 -0500

    feat: updates layout

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -40,101 +40,102 @@ export const Shop = () => {
   };
 
   return (
-    <div className=" flex items-center justify-center bg-charcoal text-cream">
-      <section className="flex gap-12 p-12 max-w-6xl w-full">
-        <div className="w-1/2 flex flex-col items-center">
-          <img
-            src={productData.image?.src}
-            alt={productData.title}
-            className="w-full max-w-md h-auto border border-cream rounded-lg mb-4"
-          />
-          <div className="flex gap-6 mb-8">
-            <div className="text-center">
-              <p className="mb-2">Vegan</p>
-              <div className="w-6 h-6 border-2 border-cream rounded-full mx-auto"></div>
-            </div>
-            <div className="text-center">
-              <p className="mb-2">Non-GMO</p>
-              <div className="w-6 h-6 border-2 border-cream rounded-full mx-auto"></div>
-            </div>
-            <div className="text-center">
-              <p className="mb-2">Gluten Free</p>
-              <div className="w-6 h-6 border-2 border-cream rounded-full mx-auto"></div>
-            </div>
-            <div className="text-center">
-              <p className="mb-2">Dairy Free</p>
-              <div className="w-6 h-6 border-2 border-cream rounded-full mx-auto"></div>
-            </div>
-            <div className="text-center">
-              <p className="mb-2">Sugar Free</p>
-              <div className="w-6 h-6 border-2 border-cream rounded-full mx-auto"></div>
-            </div>
+    <>
+      <div className="flex flex-col items-center justify-center bg-charcoal text-cream">
+        <section className="flex flex-col md:flex-row gap-12 p-12 max-w-6xl w-full">
+          {/* First Section */}
+          <div className="w-full md:w-1/2 flex flex-col items-center border border-cream rounded-lg justify-center">
+            <img
+              src={productData.image?.src}
+              alt={productData.title}
+              className="w-full max-w-md h-auto mb-4"
+            />
           </div>
-          <ContentDropdown
-            title="Ingredients & Nutrition Facts"
-            className="w-full mt-8"
-          >
-            <div>
-              <p>
-                Ingredients: Carbonated Water, Pea Protein, Natural Flavors,
-                Citric Acid, Vitamin D (Cholecalciferol), Vitamin B12
-                (Cyanocobalamin), Lemon Juice, Tangerine Extract.
-              </p>
-              <p>
-                Contains: 2.5MG THC, 5MG CBD, and 5MG minor cannabinoids from
-                full spectrum hemp extract.
-              </p>
-              <p>
-                Warning: This product is not recommended for children, people
-                pregnant or breastfeeding, or caffeine-sensitive individuals.
-              </p>
+          <div className="w-full md:w-1/2">
+            <div className="flex items-center mb-6">
+              <h2 className="text-6xl font-extrabold leading-none">
+                {productData.title}
+              </h2>
+              <div className="ml-4">
+                <IconGarnet className="w-8 h-8" />
+              </div>
             </div>
-          </ContentDropdown>
-        </div>
-        <div className="w-1/2">
-          <div className="flex items-center mb-6">
-            <h2 className="text-6xl font-extrabold leading-none">
-              {productData.title}
-            </h2>
-            <div className="ml-4">
-              <IconGarnet className="w-8 h-8" />
+            <p className="text-lg mb-8">
+              The rose has arrived in season, touting 20g of plant-derived
+              protein and 2.5mg infused thc into a one-of-a-kind beverage ready
+              for your active routine.
+            </p>
+            <div className="flex gap-4 mb-8">
+              {productData.variants.map((variant) => (
+                <Button
+                  key={variant.id}
+                  variant="outline"
+                  className="px-6 py-2 rounded-full border-2 border-cream transition-all duration-200"
+                  onClick={() => handleVariantSelection(variant)}
+                  disabled={!variant.available}
+                  isSelected={selectedVariant?.id === variant.id}
+                >
+                  {variant.title}
+                </Button>
+              ))}
             </div>
+            <p className="text-md mb-4">12 fl oz / 355 mL per can</p>
+            <p className="text-md mb-8">
+              Shipping to Minnesota, Colorado, Illinois, California
+            </p>
+            <Button
+              variant="default"
+              onClick={() => {
+                addToCart(selectedVariant.id, 1);
+                toggleCart();
+              }}
+              className="w-full py-4 mb-8 text-xl rounded-full bg-cream text-charcoal hover:bg-rose hover:text-cream"
+            >
+              ADD TO CART
+            </Button>
+            <Dietary />
           </div>
-          <p className="text-lg mb-8">
-            The rose has arrived in season, touting 20g of plant-derived protein
-            and 2.5mg infused thc into a one-of-a-kind beverage ready for your
-            active routine.
-          </p>
-          <div className="flex gap-4 mb-8">
-            {productData.variants.map((variant) => (
-              <Button
-                key={variant.id}
-                variant="outline"
-                className="px-6 py-2 rounded-full border-2 border-cream transition-all duration-200"
-                onClick={() => handleVariantSelection(variant)}
-                disabled={!variant.available}
-                isSelected={selectedVariant?.id === variant.id}
-              >
-                {variant.title}
-              </Button>
-            ))}
-          </div>
-          <p className="text-md mb-4">12 fl oz / 355 mL per can</p>
-          <p className="text-md mb-8">
-            Shipping to Minnesota, Colorado, Illinois, California
-          </p>
-          <Button
-            variant="default"
-            onClick={() => {
-              addToCart(selectedVariant.id, 1);
-              toggleCart();
-            }}
-            className="w-full py-4 mb-8 text-xl rounded-full bg-cream text-charcoal hover:bg-rose hover:text-cream"
-          >
-            ADD TO CART
-          </Button>
-        </div>
-      </section>
+        </section>
+        <section className="w-full p-10 max-w-6xl">
+          <NutritionFacts />
+        </section>
+      </div>
+    </>
+  );
+};
+
+const Dietary = () => {
+  return (
+    <div className="flex gap-6 mb-8 justify-center">
+      <div className="text-center">
+        <p className="mb-2">Vegan</p>
+        <div className="w-6 h-6 border-2 border-cream rounded-full mx-auto"></div>
+      </div>
+      <div className="text-center">
+        <p className="mb-2">Non-GMO</p>
+        <div className="w-6 h-6 border-2 border-cream rounded-full mx-auto"></div>
+      </div>
+      <div className="text-center">
+        <p className="mb-2">Gluten Free</p>
+        <div className="w-6 h-6 border-2 border-cream rounded-full mx-auto"></div>
+      </div>
+      <div className="text-center">
+        <p className="mb-2">Dairy Free</p>
+        <div className="w-6 h-6 border-2 border-cream rounded-full mx-auto"></div>
+      </div>
+      <div className="text-center">
+        <p className="mb-2">Sugar Free</p>
+        <div className="w-6 h-6 border-2 border-cream rounded-full mx-auto"></div>
+      </div>
     </div>
+  );
+};
+
+const NutritionFacts = () => {
+  return (
+    <ContentDropdown
+      title="Ingredients & Nutrition Facts"
+      className="w-full mt-8"
+    />
   );
 };
