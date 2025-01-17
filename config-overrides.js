cc20807f91c1470149c12cfdce4bdfddaf80a718
Austin Barrett <austinjbarrett98@gmail.com>
2025-01-17 00:47:58 -0500

    feat: adds API module

--- config-overrides.js
+++ config-overrides.js
@@ -4,6 +4,7 @@ module.exports = function override(config) {
   config.resolve.alias = {
     ...config.resolve.alias,
     "@assets": path.resolve(__dirname, "src/assets"),
+    "@api": path.resolve(__dirname, "src/api"),
     "@common": path.resolve(__dirname, "src/common"),
     "@theme": path.resolve(__dirname, "src/components/theme"),
     "@components": path.resolve(__dirname, "src/components/common"),
